26045972a42c60927710b2f17af4c6e934526205
Yia <Yia1095101851@outlook.com>
2022-01-21 20:57:03 +0800

    python exercise

---
+++ turtlefan.py
@@ -0,0 +1,9 @@
+import turtle   as  t
+for i in range(4):
+    t.left(45)
+    t.fd(50)
+    t.left(90)
+    t.circle(50,45)
+    t.left(90)
+    t.fd(50)
+    t.left(180)    
---
+++ turtlemobiwusi.py
@@ -0,0 +1,4 @@
+import turtle   as  t
+for i in range(9):
+    t.fd(50)
+    t.left(80)
